cdc2c7d7d57c0adf27eecbd2d967faf582952175
Iva Pleic <ipleic46@gmail.com>
2025-08-28 11:14:22 +0200

    Added ignore build error in config file

--- next-reads/next.config.ts
+++ next-reads/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['i.pravatar.cc','m.media-amazon.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
